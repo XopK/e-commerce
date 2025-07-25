92dd7be8ffa6db4fcec1b280fc2d13a4e898a4f1
XopK <dmahmutov12@gmail.com>
2025-07-26 03:55:47 +0500

    start header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,12 +2,16 @@
 </script>
 
 <template>
-<header>
-    <div class="logo">
-        
-    </div>
-</header>
+    <header class="w-full flex px-25 py-6">
+        <div class="logo text-3xl font-integral font-bold">
+            <a href="/">
+                <h1 class="uppercase select-none">Shop.co</h1>
+            </a>
+        </div>
+
+        <nav>
+        </nav>
+    </header>
 </template>
 
-<style>
-</style>
+<style></style>
