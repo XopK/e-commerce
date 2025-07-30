6d4878e80eac9092849629ca85ebf49214c2b11b
XopK <dmahmutov12@gmail.com>
2025-07-31 03:23:55 +0500

    index hero section

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -2,7 +2,38 @@
 </script>
 
 <template>
+    <div class="hero px-25 py-25">
+        <div class="hero-text w-xl">
+            <h2 class="uppercase font-integral font-bold text-6xl">Find clothes that matches your style</h2>
+
+            <p class="font-satoshi font-normal opacity-60 mt-8">Browse through our diverse range of meticulously crafted
+                garments, designed to bring
+                out your individuality and cater to your sense of style.</p>
+
+            <button class="btn btn-primary mt-8 font-satoshi font-medium">Shop Now</button>
+
+            <div class="more-hero-text mt-10">
+                <ul class="flex items-center">
+                    <li>
+                        200+
+                        <span>International Brands</span>
+                    </li>
+                    <li>
+                        200+
+                        <span>International Brands</span>
+                    </li>
+                    <li>
+                        200+
+                        <span>International Brands</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped>
+.hero {
+    background-color: #F2F0F1;
+}
 </style>
